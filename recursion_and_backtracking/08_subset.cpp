0b6c2f59ec367854f3ef90a211ad7623c2b7d7fd
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-04-06 23:53:26 +0530

    making subset through recursion

---
+++ recursion_and_backtracking/08_subset.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve(string ip, string op)
+{
+    if (ip.length() == 0)
+    {
+        cout << op << " ";
+    }
+    string op1 = op;
+    string op2 = op;
+    op2.push_back(ip[0]);
+    ip.erase(ip.begin() + 0);
+    solve(ip, op1);
+    solve(ip, op2);
+}
+int main()
+{
+    string ip;
+    cin >> ip;
+    string op = "";
+    solve(ip, op);
+    return 0;
+}
