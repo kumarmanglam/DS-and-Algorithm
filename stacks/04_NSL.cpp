b8beef642e07da25a2886ba713e21d9c1691003d
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-04-14 18:49:37 +0530

    nearest smaller to element code done with practicing

---
+++ stacks/04_NSL.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+vector <int> v;
+stack <int> s;
+void nextsmaller(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        if (s.size()==0)
+            v.push_back(-1);
+        else if(s.size()>0 && s.top()<arr[i])
+            v.push_back(s.top());
+        else if(s.size()>0 && s.top()>arr[i])
+        {
+            while (s.size()>0 && s.top()>arr[i])
+                s.pop();
+            if (s.size()==0)
+                v.push_back(-1);
+            else v.push_back(s.top());
+        }
+    s.push(arr[i]);
+    }
+}
+int main(){
+    int arr[100]={4,5,2,10,8};
+    int size=5;
+    nextsmaller(arr,size);
+    for(int i=0;i<size;i++)
+        cout<<v[i]<<"|";
+    return 0;
+}
