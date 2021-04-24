511c42d017e6a64f17bc2d9d6de3e8835000a60e
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-04-24 22:50:47 +0530

    done

---
+++ sliding_window/02_first-negative.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+list <int> l;
+vector <int> v;
+void first_negative(int arr[],int size,int k)
+{
+    int i=0,j=0;
+    while (j<size)
+    {
+        while ((j-i+1)<k)
+        {
+            if (arr[j]<0)
+                l.push_back(arr[j]);
+            j++;
+        }
+        if ((j-i+1)==k)
+        {
+            if (arr[j]<0)
+                l.push_back(arr[j]);
+            if (l.size()==0)
+                v.push_back(0);
+            else
+            {
+                v.push_back(l.front());
+                if(arr[i]==l.front())
+                    l.pop_front();
+            }
+        i++;
+        j++;
+        }
+    }
+}
+int main()
+{
+    int arr[]={12,-1,-7,8,-1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=3;
+    first_negative(arr,n,k);
+    for(int i=0;i<3;i++)
+    {
+        cout<<v[i]<<" | ";
+    }
+    cout<<endl;
+    return 0;
+}
