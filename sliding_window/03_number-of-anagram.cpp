1fb36f5fc24321ee7183698d8ed08efbf44226a8
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-04-25 23:47:25 +0530

    incomplete

--- sliding_window/03_number-of-anagram.cpp
+++ sliding_window/03_number-of-anagram.cpp
@@ -3,37 +3,38 @@
 #include <string>
 #include <vector>
 using namespace std;
-map <char,int> mp;
-int anagram(string s, int size,int k)
+map<char, int> mp;
+int anagram(string s, int size, int k)
 {
-    int i=0,j=0,count=3,ans=0;
-    while (j<size)
+    //value of count is equal to number of unique elements in pattern string
+    int i = 0, j = 0, count = 2, ans = 0;
+    while (j < size)
     {
-        while ((j-i+1)<k)
+        while ((j - i + 1) < k)
         {
-            if (mp.count(s[j]>0))
+            if (mp.count(s[j] > 0))
             {
                 mp[s[j]]--;
             }
             j++;
         }
-        if((j-i+1)==k)
+        if ((j - i + 1) == k)
         {
-            if(mp.count(s[j]))
+            if (mp.count(s[j]) > 0)
             {
                 mp[s[j]]--;
             }
-            for(map<char,int>::iterator j=mp.begin();j!=mp.end();++j)
+            for (map<char, int>::iterator j = mp.begin(); j != mp.end(); ++j)
             {
-                if((*j).second==0)
+                if ((*j).second == 0)
                     count--;
             }
-            if(count==0)
+            if (count == 0)
                 ans++;
         }
         //now we have to remove start of window and do the calculation!!!
         //we have to reset count also but what should be the condition!!
-        if(mp.count(s[i]))
+        if (mp.count(s[i]) > 0)
         {
             mp[s[i]]++;
             count++;
@@ -46,22 +47,22 @@ int anagram(string s, int size,int k)
 
 int main()
 {
-    string ptr,ss;
-    ptr="aabaabaa";
-    ss="aaba";
-    int size,sizz;
-    size=sizeof(ptr)/sizeof(ptr[0]);
-    sizz=sizeof(ptr)/sizeof(ptr[0]);
-    for (int i=0;i<4;i++)
+    string ptr, ss;
+    ptr = "abbabbaa";
+    ss = "abba";
+    int size, sizz;
+    size = sizeof(ptr) / sizeof(ptr[0]);
+    sizz = sizeof(ptr) / sizeof(ptr[0]);
+    for (int i = 0; i < 4; i++)
     {
         mp[ss[i]]++;
     }
-    for(map<char,int>::iterator j=mp.begin();j!=mp.end();++j)
+    for (map<char, int>::iterator j = mp.begin(); j != mp.end(); ++j)
     {
-        cout<<(*j).first<<" - "<<(*j).second<<endl;
+        cout << (*j).first << " - " << (*j).second << endl;
     }
     int anss;
-    anss=anagram(ptr,size,sizz);
-    cout<<"the answer is  "<<anss<<endl;
+    anss = anagram(ptr, size, sizz);
+    cout << "the answer is  " << anss << endl;
     return 0;
 }
