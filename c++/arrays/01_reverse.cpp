486bd288499aad041ec54c5d30405e5c22607ab8
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-02 22:32:53 +0530

    reverse algo updated with start and end index

--- c++/arrays/01_reverse.cpp
+++ c++/arrays/01_reverse.cpp
@@ -7,13 +7,28 @@ void reverse(int arr[],int n)
         swap(arr[i],arr[n-1-i]);
     }   
 }
-int main()
-{
-    int arr[100]={1,2,3,4,5};
-    reverse(arr,5);
-    for(int i=0;i<5;i++)
-    {
+void reverseSE(int arr[],int start,int end){
+    for(int i=0;i<=(end-start)/2;i++){
+        swap(arr[start+i-1],arr[end-i-1]);
+    }
+}
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-    return 0; 
+    cout<<endl;
 }
+int main()
+{
+    int arr[100]={1,2,3,4,5,6,7,8,9};
+    // reverse(arr,5);
+    reverseSE(arr,3,5);
+    printArray(arr,9);
+    // cout<<(5-2)/2;
+    return 0;
+}
+//end-start/2=1
+//i=0
+//s+i-1=1,d-i-1=4
+//i=1
+//s+i-1=2,d-i-1=3
