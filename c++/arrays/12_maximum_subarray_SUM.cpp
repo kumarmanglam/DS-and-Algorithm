3441db85d6dfc376e488e41f22eb0363af08000d
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-06 02:26:15 +0530

    to be understood in dee[

--- c++/arrays/12_maximum_subarray_SUM.cpp
+++ c++/arrays/12_maximum_subarray_SUM.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <climits>
 using namespace std;
+//my  naive approach
 int maxiSubSum(int arr[],int n){
     int mx=INT_MIN,sum=INT_MIN;
     for(int i=0;i<n;i++){
@@ -12,6 +13,18 @@ int maxiSubSum(int arr[],int n){
         }
     }
     return mx;
+}
+//dp approach
+int maxSubArraySum(int a[], int size)
+{
+    int max_so_far = a[0];
+    int sum= a[0];
+    for (int i = 1; i < size; i++)
+    {
+        sum = max(a[i], sum+a[i]);
+        max_so_far = max(max_so_far, sum);
+    }
+    return max_so_far;
 }
 int main(){
     int arr[]={1,2,3,-2,5};
--- c++/arrays/13_KADANES_MAXIMUMsumofsubarray.cpp
+++ c++/arrays/13_KADANES_MAXIMUMsumofsubarray.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int kadane(int arr[],int size){
-    int mxsofar=INT_MIN,sum=INT_MIN;
+    int mxsofar=INT_MIN,sum=0;
     for(int i=0;i<size;i++){
         sum=sum+arr[i];
         mxsofar=max(sum,mxsofar);
@@ -14,7 +14,7 @@ int kadane(int arr[],int size){
     return mxsofar;
 }
 int main(){
-    int arr[]={1,2,3,-2,5};
+    int arr[]={23 ,41 ,84, -8 ,42, -54, 1 ,2 ,28 ,49 ,-32 ,-16 ,-33, -44 ,-100, -30, 68 ,-47 ,59 ,-94, 35 ,-18};
     int n=sizeof(arr)/sizeof(arr[0]);
     int ans=kadane(arr,n);
     cout<<ans<<" ";
