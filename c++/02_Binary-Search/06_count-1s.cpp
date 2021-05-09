9320cffcc9a21a631875fccc1280a337cd1d2504
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-09 07:29:14 +0530

    ok

--- c++/02_Binary-Search/06_count-1s.cpp
+++ c++/02_Binary-Search/06_count-1s.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 using namespace std;
-int firstiter(int arr[],int n){
+//GFG code
+int onesinBN(int arr[],int n){
     int low=0;
     int high=n-1;
-    int mid=(high+low)/2;
-    while(high>low){
+    while(low<=high){  
+        int mid=(high+low)/2;
         if(arr[mid]==1){
-            if(mid==0 || arr[mid-1]!=1){
-                return mid;
+            if(mid==0 || arr[mid-1]==0){
+                return n-mid;
+            }
+            else {
+                high=mid-1;
             }
         }
-        else if(arr[mid]<1){
+        else{
             low=mid+1;
         }
     }
-    return -1;
+    return 0;
+}
+//my code
+int firstiter(int arr[],int n){
+    int low=0;
+    int high=n-1;
+    while(low<=high){  
+        int mid=(high+low)/2;
+        if(arr[mid]==1){
+            if(mid==0 || arr[mid-1]==0){
+                return mid;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return 0;
 }
 int no1s(int arr[],int n){
     if(firstiter(arr,n) == -1){
@@ -23,8 +47,8 @@ int no1s(int arr[],int n){
     else return n-firstiter(arr,n);
 }
 int main(){
-    int arr[]={0,0,0,1,1,1,1};
+    int arr[]={0,1,1,1,1,1,1};
     int n=sizeof(arr)/sizeof(arr[0]);
-    cout<<no1s(arr,n)<<endl;
+    cout<<onesinBN(arr,n)<<endl;
   return 0;
 }
