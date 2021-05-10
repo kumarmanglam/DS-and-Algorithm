8685f39e86dc013ff18c1d48a8cb0c4f6177c865
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-10 11:44:40 +0530

    bitwise operators

---
+++ c++/03_bit_magic/01_bitwise-operators.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=3,y=6;
+    cout<<(x&y)<<endl;//and operator
+    cout<<(x|y)<<endl;//or operator
+    cout<<(x^y)<<endl;//Xor operator
+    return 0;
+}
---
+++ c++/03_bit_magic/02_bitwise_operators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //left shift operator
+    cout<<("left shift operator ")<<endl;
+    int x=3;
+    cout<<(x<<1)<<endl;
+    cout<<(x<<2)<<endl;
+    int y=4;
+    int z=(x<<y);
+    cout<<z<<endl;
+
+    //right shift operator
+    cout<<("right shift operator ")<<endl;
+    int a=33;
+    cout<<(a>>1)<<endl;
+    cout<<(a>>2)<<endl;
+    int b=4;
+    int c=(a>>y);
+    cout<<z; 
+    //bitwise not operator
+    cout<<("bitwise not operator ")<<endl;
+    int m=1;
+    cout<<(~m)<<endl
+    m=5;
+    cout<<(~m)<<endl;
+    return 0;
+}
