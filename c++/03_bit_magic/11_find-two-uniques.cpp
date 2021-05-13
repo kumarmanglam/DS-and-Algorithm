24f905b730b34fddf719bce8f0be6703e51abfb1
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-13 15:15:22 +0530

    find uniques by bit magic

---
+++ c++/03_bit_magic/11_find-two-uniques.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int getBit(int n,int pos){
+    return ((n&(1<<pos))!=0);
+}
+
+int Twouniques(int arr[],int n){
+    int xorsum=0;
+    for(int i=0;i<n;i++){
+        xorsum^=arr[i];
+    }
+    int setbit=0,pos=0,temp=0;
+    temp=xorsum;
+    //mismatched position 
+    while(setbit!=1){
+        setbit=xorsum&1;
+        pos++;
+        xorsum=xorsum>>1;
+    }//jab loop se niklenge ek pos extra add ho jygi
+    int newxor=0;
+    //mismatched position ki respect m puri
+    //loop chalegi taki ek unique mil jaye
+    for(int i=0;i<n;i++){
+        if(getBit(arr[i],pos-1)){
+            newxor^=arr[i];
+        }
+    }
+    cout<<newxor<<endl;
+    //doosra unique aise niklega
+    cout<<(temp^newxor)<<endl;
+}
+
+int main(){
+    int arr[]={2,2,3,3,4,4,5,6};
+    Twouniques(arr,8);
+    return 0;
+}
