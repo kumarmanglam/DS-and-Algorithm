852cbef51ef7041510386b307da9404482dd344d
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-15 14:35:03 +0530

    Update 05_Check_subsequence.cpp

--- c++/04_strings/05_Check_subsequence.cpp
+++ c++/04_strings/05_Check_subsequence.cpp
@@ -7,9 +7,10 @@ int checksubsequence(string st1,string st2){
 	for(int i=0;i<st1.length() && j<st2.length();i++){
 		if(st1[i]==st2[j]
 		   j++;
-	return (j==st2.length);
-		  
+	return (j==st2.length);	   
+}
 int main(){
 	string st1="geeksforgeeks";
 	string st2="grges";
 	cout<<checksubsequence(st1,st2);
+}
