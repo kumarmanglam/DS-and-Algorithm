18fcd5cb53664a3f21764f3b8a0fab7f35ac8617
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-15 13:19:33 +0530

    Create 03_operators_replaces_strcmp.cpp
    using operator from string class instead of using strcmp

---
+++ c++/04_strings/03_operators_replaces_strcmp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+string s1="abc";
+string s2="bgf;
+  if(s1==s2){
+    cout<<"same";
+  else if (s1<s2)
+    cout<<"less";
+  else
+      cout<<"greater" ;
+}//using operator from string class instead of using strcmp
