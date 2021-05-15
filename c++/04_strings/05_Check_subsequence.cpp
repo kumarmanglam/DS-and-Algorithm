1c1965aa33610bad0c30e849554e40f32136183a
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-15 18:17:14 +0530

    done

--- c++/04_strings/05_Check_subsequence.cpp
+++ c++/04_strings/05_Check_subsequence.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#inlcude <string>
+#include <string>
 using namespace std;
 
 int checksubsequence(string st1,string st2){
 	int j=0;
 	for(int i=0;i<st1.length() && j<st2.length();i++){
-		if(st1[i]==st2[j]
-		   j++;
-	return (j==st2.length);	   
+		if(st1[i]==st2[j])
+			j++;
+	}
+	return (j==st2.length());	   
 }
+
 int main(){
 	string st1="geeksforgeeks";
 	string st2="grges";
