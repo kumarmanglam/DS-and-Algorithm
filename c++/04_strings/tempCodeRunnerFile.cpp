551acc94b802e520082984469872501e7ff3212e
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-16 01:44:10 +0530

    ok

--- c++/04_strings/tempCodeRunnerFile.cpp
+++ c++/04_strings/tempCodeRunnerFile.cpp
@@ -1,14 +1,2 @@
-ol areangrams(string s1,string s2){
-//     if(s1.length()!=s2.length())
-//         return false;
-//     int count[CHAR]={0};
-//     for(int i=0;i<s1.length();i++){
-//         count[s1[i]]++;
-//         count[s2[i]]--;
-//     }
-//     for(int i=0;i<CHAR;i++){
-//         if(count!=0)    
-//             return false;
-//     }
-//     return true
-// }
+
+// one string element will add a vote and another will delete a vote
