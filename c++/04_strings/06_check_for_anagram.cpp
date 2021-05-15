3109638a49b3bce1bef5f7906d8b6bc6599b2345
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-16 01:44:04 +0530

    ok

--- c++/04_strings/06_check_for_anagram.cpp
+++ c++/04_strings/06_check_for_anagram.cpp
@@ -1,39 +1,36 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 //naive solution using inbuilt function -- O(nlogn)time
 bool areanagram(string s1,string s2){
-    if(s1.length()!=s2.length()){
-        if(s1.length()!=s2.length())
-            return false;
-        sort(s1.length(),s1.length());
-        sort(s2.length(),s2.length());
-        return (s1==s2);
-    }
+    if(s1.length()!=s2.length())
+        return false;
+    sort(s1.begin(),s1.end());
+    sort(s2.begin(),s2.end());
+    return (s1==s2);
 }
 
-//efficient solution logic is like moore voting algorithm
-//one string element will add a vote and another will delete a vote
-// bool areangrams(string s1,string s2){
-//     if(s1.length()!=s2.length())
-//         return false;
-//     int count[CHAR]={0};
-//     for(int i=0;i<s1.length();i++){
-//         count[s1[i]]++;
-//         count[s2[i]]--;
-//     }
-//     for(int i=0;i<CHAR;i++){
-//         if(count!=0)    
-//             return false;
-//     }
-//     return true
-// }
+
+// efficient solution logic is like moore voting algorithm
+// one string element will add a vote and another will delete a vote
+bool anagrams(string s1,string s2){
+    if(s1.length()!=s2.length())
+        return false;
+    int count[CHAR]={0};
+    for(int i=0;i<s1.length();i++){
+        count[s1[i]]++;
+        count[s2[i]]--;
+    }
+    for(int i=0;i<CHAR;i++){
+        if(count[i]!=0)    
+            return false;
+    }
+    return true;
+}//debugging left in this code ->4 errors
 
 int main(){
     string s1="AABA";
     string s2="ABAA";
-    cout<<areanagram(s1,s2);
+    cout<<anagrams(s1,s2);
     return 0;
 }
