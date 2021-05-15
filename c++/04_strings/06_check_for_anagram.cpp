66aa951b4ee1c3d9d6c961f43105a82975b643a2
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-16 01:34:35 +0530

    lol

--- c++/04_strings/06_check_for_anagram.cpp
+++ c++/04_strings/06_check_for_anagram.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 //naive solution using inbuilt function -- O(nlogn)time
@@ -12,24 +13,27 @@ bool areanagram(string s1,string s2){
         return (s1==s2);
     }
 }
+
 //efficient solution logic is like moore voting algorithm
 //one string element will add a vote and another will delete a vote
-bool areangrams(string s1,string s2){
-    if(s1.length()!=s2.length())
-        return false;
-    int count[CHAR]={0};
-    for(int i=0;i<s1.length();i++){
-        count[s1[i]]++;
-        count[s2[i]]--;
-    }
-    for(int i=0;i<CHAR;i++){
-        if(count!=0)    
-            return false;
-    }
-    return true
-}
+// bool areangrams(string s1,string s2){
+//     if(s1.length()!=s2.length())
+//         return false;
+//     int count[CHAR]={0};
+//     for(int i=0;i<s1.length();i++){
+//         count[s1[i]]++;
+//         count[s2[i]]--;
+//     }
+//     for(int i=0;i<CHAR;i++){
+//         if(count!=0)    
+//             return false;
+//     }
+//     return true
+// }
+
 int main(){
-
-
+    string s1="AABA";
+    string s2="ABAA";
+    cout<<areanagram(s1,s2);
     return 0;
 }
---
+++ c++/04_strings/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+ol areangrams(string s1,string s2){
+//     if(s1.length()!=s2.length())
+//         return false;
+//     int count[CHAR]={0};
+//     for(int i=0;i<s1.length();i++){
+//         count[s1[i]]++;
+//         count[s2[i]]--;
+//     }
+//     for(int i=0;i<CHAR;i++){
+//         if(count!=0)    
+//             return false;
+//     }
+//     return true
+// }
