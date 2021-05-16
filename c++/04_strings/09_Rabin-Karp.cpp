46dd7628aa7b1c17af3bad2e4463325f1fe1b4da
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-17 00:12:27 +0530

    rabinkarp updated

--- c++/04_strings/09_Rabin-Karp.cpp
+++ c++/04_strings/09_Rabin-Karp.cpp
@@ -2,8 +2,43 @@
 using namespace std;
 #define d 256
 const int q=101;
-//Rolling has solution O((n-m+1)*m) in worst case
+
+//m1--Rolling has solution O((n-m+1)*m) in worst case
+//this is efficient approach where sum of value is done btw 
+//here we apply hashing and sliding window logic together
 void RBSearch(string pat,string txt,int M, int N){
+    //Compute (d^(M-1))%q
+    int h=1;
+    for(int i=1;i<=M-1;i++)
+        h=(h*d)%q;
+    
+    //Compute p and to
+    int p=0,t=0;
+    for(int i=0;i<M;i++){
+        p=(p*d+pat[i])%q;
+        t=(t*d+txt[i])%q;
+    }
+    
+    //Check for spurious hit
+    for(int i=0;i<=(N-M);i++){
+        if(p==t){
+            bool flag=true;
+            for(int j=0;j<M;j++)
+                if(txt[i+j]!=pat[j]){flag=false;break;}
+            if(flag==true)cout<<i<<" ";
+        }
+        //Compute ti+1 using ti
+        if(i<N-M){
+            t=(t+txt[i+M]-txt[i])%q;
+            if(t<0)t=t+q;
+        }
+    }
+}
+
+//m2--Rolling has solution O((n-m+1)*m) in worst case
+//this is efficient approach where weighted sum is done btw 
+//here we apply hashing and sliding window logic together
+void RBSearchm2(string pat,string txt,int M, int N){
     
     //Compute (d^(M-1))%q
     int h=1;
@@ -24,13 +59,12 @@ void RBSearch(string pat,string txt,int M, int N){
             for(int j=0;j<M;j++)
                 if(txt[i+j]!=pat[j]){flag=false;break;}
             if(flag==true)cout<<i<<" ";
-    }
-
-    //Compute ti+1 using ti
-    if(i<N-M){
-        t=((d*(t-txt[i]*h))+txt[i+M])%q;
-        if(t<0)t=t+q;
-    }
+        }
+        //Compute ti+1 using ti
+        if(i<N-M){
+            t=((d*(t-txt[i]*h))+txt[i+M])%q;
+            if(t<0)t=t+q;
+        }
     }
     
 }
