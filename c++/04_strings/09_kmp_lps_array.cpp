78b2d500f360ac9e0463dcad562756b3eff80f9e
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-17 10:22:05 +0530

    Create 09_kmp_lps_array.cpp
    created lps array which useful in finding a pattern in o(n)

---
+++ c++/04_strings/09_kmp_lps_array.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+//method 1 is naive solution taking o(n^3) time 
+int longPropPreSuff(string str, int n){
+    for(int len=n-1;len>0;len--){
+        bool flag=true;
+        for(int i=0;i<len;i++)
+            if(str[i]!=str[n-len+i])
+                flag=false;
+                
+        if(flag==true)
+            return len;
+    }
+    return 0;
+}
+
+void fillLPS(string str, int *lps){
+    for(int i=0;i<str.length();i++){
+        lps[i]=longPropPreSuff(str,i+1);
+    }
+}
+//mthod 2 is efficient solution o(N) time
+void fillLPS(string str, int *lps){
+    int n=str.length(),len=0;
+    lps[0]=0;
+    int i=1;
+    while(i<n){
+        if(str[i]==str[len])
+        {len++;lps[i]=len;i++;}
+        else
+        {if(len==0){lps[i]=0;i++;}
+            else{len=lps[len-1];}
+        }
+    }
+}
+ 
+int main() 
+{ 
+    string txt = "abacabad";int lps[txt.length()];
+    fillLPS(txt,lps);
+    for(int i=0;i<txt.length();i++){
+        cout<<lps[i]<<" ";
+    }
+    
+    return 0; 
+} 
