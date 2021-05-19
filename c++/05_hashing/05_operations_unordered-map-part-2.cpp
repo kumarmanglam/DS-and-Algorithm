c85d07c5bb0056cac5f4334da4eed04a18adb0e7
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-19 23:33:36 +0530

    oops

---
+++ c++/05_hashing/05_operations_unordered-map-part-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    unordered_map <string,int>m;
+    m["gfg"]=20;//this operator will insert the key if that is not present earlier
+    m["ide"]=30;
+    m["map"]=15;
+
+    //count function return 0 or 1
+
+
+}
