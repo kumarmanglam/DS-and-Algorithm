56a6b258484ba091918c75c348d7c97e288288b7
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-20 19:11:45 +0530

    using unordered set to find the subarray with given sum

--- c++/05_hashing/12_given_sum_subarray.cpp
+++ c++/05_hashing/12_given_sum_subarray.cpp
@@ -16,27 +16,45 @@ int givensum(int arr[],int n,int x){
     return false;
 }
 
-//efficient solution --O(n)--
-int givensums(int arr[],int n,int x){
-    unordered_set<int>h;
-    int presum1=0;
-    int presum2=0;
+//efficient solution by gfg
+//in this approach logic is that we are checking current_sum - required_sum constantly throughout the loop
+//example 
+//array -      5  8  6  13  3  -1
+//prefix sum-  5  13 
+int findsumsubarray(int arr[],int n,int sum){
+    unordered_set <int>h;
+    int pre_sum=0;
     for(int i=0;i<n;i++){
-        presum2=presum1;
-        presum2+=arr[i];
-        
-        if(h.find(presum2-presum1)!=h.end())
-            return true ;
-        if(presum1==x)
+        pre_sum+=arr[i];
+        if(pre_sum==sum)
             return true;
-        h.insert(presum2);
+        if(h.find(pre_sum-sum)!=h.end())
+            return true;
+        h.insert(pre_sum);
     }
     return false;
 }
-
 int main(){
     int arr[]={0,12,1,-4,10,3};
     int n=sizeof(arr)/sizeof(arr[0]);
-    cout<<givensum(arr,n,7);
+    cout<<findsumsubarray(arr,n,7);
     return 0;
 }
+
+// //efficient solution --O(n)--
+// int givensums(int arr[],int n,int x){
+//     unordered_set<int>h;
+//     int presum1=0;
+//     int presum2=0;
+//     for(int i=0;i<n;i++){
+//         presum2=presum1;
+//         presum2+=arr[i];
+        
+//         if(h.find(presum2-presum1)!=h.end())
+//             return true ;
+//         if(presum1==x)
+//             return true;
+//         h.insert(presum2);
+//     }
+//     return false;
+// }
