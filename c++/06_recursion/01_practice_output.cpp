0848ee3cf3e16c3f9ba434c32eb88fe421c3b056
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-21 21:54:59 +0530

    Create 01_practice_output.cpp
    illustrates the process of output in recursion

---
+++ c++/06_recursion/01_practice_output.cpp
@@ -0,0 +1,21 @@
+  #include <iostream>
+using namespace std;
+
+
+void fun1(int n)
+{
+	if(n == 0)
+		return;
+
+	cout<<n<<endl;
+
+	fun1(n - 1);
+	
+	cout<<n<<endl;
+}
+int main() {
+	
+	fun1(3);
+	
+	return 0;
+}
