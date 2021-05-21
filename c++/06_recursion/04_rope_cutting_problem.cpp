0126a2fc0da96e5831161811d871255a640a8534
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-22 02:15:01 +0530

    rope cutting recursive tree problem

---
+++ c++/06_recursion/04_rope_cutting_problem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int ropetask(int n,int a,int b,int c){
+    if(n<0)
+        return false;
+    if(n==0)
+        return true;
+    return(n-a,a,b,c);
+    return(n-b,a,b,c);
+    return(n-c,a,b,c);
+}
+
+int main(){
+    ropetask(9,2,3,1);
+
+    return 0;
+}
