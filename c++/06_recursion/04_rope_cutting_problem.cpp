e5571959ee26da9b289d3612373305f969e100f8
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-22 15:04:24 +0530

    recursion

--- c++/06_recursion/04_rope_cutting_problem.cpp
+++ c++/06_recursion/04_rope_cutting_problem.cpp
@@ -1,17 +1,25 @@
+//to be vesited
+//given as rope cut the rope into maximum 
+//number of peices such that every peice has length either a,b,c
+
 #include <iostream>
 using namespace std;
-int ropetask(int n,int a,int b,int c,int r){
+int r=0;
+
+int ropetask(int n,int a,int b,int c){
     if(n<0)
         return -1;
     if(n==0)
         return 0;
-    int r=max(ropetask(n-a,a,b,c,r),ropetask(n-b,a,b,c,r),ropetask(n-c,a,b,c,r));
-    if(r==-1)
+    int k=-900,r=-900;
+    k=max(ropetask(n-a,a,b,c),ropetask(n-b,a,b,c));
+    r=max(r,ropetask(n-c,a,b,c));
+    if(r==(-1))
         return -1;
     return r+1;
 }
 
 int main(){
-    cout<<ropetask(9,2,2,2,0);
+    cout<<ropetask(9,2,2,3);
     return 0;
 }
