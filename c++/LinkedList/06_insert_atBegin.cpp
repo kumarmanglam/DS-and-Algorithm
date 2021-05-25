d5f4ef03d79c0a7c3e0b5921904d0add05c9dde0
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-25 22:10:40 +0530

    Update 06_insert_atBegin.cpp

--- c++/LinkedList/06_insert_atBegin.cpp
+++ c++/LinkedList/06_insert_atBegin.cpp
@@ -1,40 +1,25 @@
-//do not copy the code directly
-//insert at begin of linked list;
-
 #include <iostream>
 using namespace std;
 
 struct node{
     int data;
-    node *next;
-    
+    node* next;
     node(int x){
         data=x;
         next=NULL;
     }
-};
-
-void printlist(node * head){
-    if(head==NULL){
-        return;
-    }
-    cout<<head->data<<" -> ";
-    printlist(head->next);
 }
 
-node* insert(node *head, int x){
-    node *newly=new node(x);        //created a node to data x
-    node *temp=head;                //copied address of head to another pointer temp
-    head=newly;                     //linked address of newnode(newly) to head
-    newly->next=temp->next;         //made previous first node to new node newly 
-    return newly;
+node* insertatbegin(node *first,int x){
+    node temp=new node(x);
+    temp->next=head;
+    return head;
 }
 
 int main(){
     node* head=new node(4);
     node*first=new node(8);
     node* second=new node(29);
-    
     head->next=first;
     first->next=second;
     
