a8ed278a892f8dec2141228aeffa3c57dc16e60d
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-26 02:16:59 +0530

    updated some comments

--- c++/LinkedList/10_insert_any_pos.cpp
+++ c++/LinkedList/10_insert_any_pos.cpp
@@ -9,7 +9,6 @@ struct node{
         next=NULL;
     }
 };
-//update condition for invalid index input
 node* insertatindex(node*head,int data,int i){
     node*newly=new node(data);              //new node
     if(i==1){                               //if insert is at beginning
@@ -18,26 +17,17 @@ node* insertatindex(node*head,int data,int i){
     }
     node*temp=head;                         //temp copy of head
     int count=1;
-    while(count<i-1 && temp->next!=NULL){                   //we have to run the loop till we reach the position minus two position
+    while(count<i-1 && temp!=NULL){     //we have to run the loop till we reach the position minus two position coz pichle iteration m hi hmne temp to temp=temp_next kr diya tha
         temp=temp->next;
         count++;
     }
-    if(temp==NULL){                    //redundant but why??????
+    if(temp==NULL){                   
         return NULL;
     }
-    newly->next=temp->next;                     //first we need to link second bridge node to newly node
+    newly->next=temp->next;                     //first we need to link second bridge node to newly node valid for end of the linked list also
     temp->next=newly;
     return head;
 }
-
-void printlist(node* head){
-    node*temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    }
-}
-
 int main(){
     node*head=new node(10);
     head->next=new node (20);
