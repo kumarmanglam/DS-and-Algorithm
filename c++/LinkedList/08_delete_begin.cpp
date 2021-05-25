cba99ebfa0110eb48cc39479d16f4628b3b44df6
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-25 23:57:48 +0530

    in cpp we have to deallocate memory for deleting

--- c++/LinkedList/08_delete_begin.cpp
+++ c++/LinkedList/08_delete_begin.cpp
@@ -9,13 +9,16 @@ struct node{
         next=NULL;
     }
 };
-
+//we have to care of memory deallocation
 node * deletebegin(node*head){
-    node*temp=head;
-    temp=head->next;
-    return temp;
+    if(head==NULL)  
+        return NULL;
+    else{
+        node*temp=head->next;
+        delete head;
+        return temp;
+    }
 }
-
 int main() {
     node*head=new node(1);
     head->next=new node(2);
@@ -24,3 +27,11 @@ int main() {
     cout<<head->data;
 	return 0;
 }
+
+
+
+// java program
+// node *deletehead(node head){
+//     if(head==null)
+//         return null;
+//     return head->next;
