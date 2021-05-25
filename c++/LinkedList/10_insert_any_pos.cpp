1b186000974016a3338e950aeed5fbf4c60d60a2
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-26 02:43:50 +0530

    comments updated to better understand the code

--- c++/LinkedList/10_insert_any_pos.cpp
+++ c++/LinkedList/10_insert_any_pos.cpp
@@ -9,6 +9,7 @@ struct node{
         next=NULL;
     }
 };
+//update condition for invalid index input
 node* insertatindex(node*head,int data,int i){
     node*newly=new node(data);              //new node
     if(i==1){                               //if insert is at beginning
@@ -21,18 +22,25 @@ node* insertatindex(node*head,int data,int i){
         temp=temp->next;
         count++;
     }
-    if(temp==NULL){                   
-        return NULL;
+    if(temp==NULL){                     // temp=newly; should not be added in this code because it is case which handles invalid case input
+        return head;
     }
     newly->next=temp->next;                     //first we need to link second bridge node to newly node valid for end of the linked list also
     temp->next=newly;
     return head;
 }
+
+void printlist(node* head){
+    node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
 int main(){
     node*head=new node(10);
-    head->next=new node (20);
-    head->next->next=new node(30);
-    head->next->next->next==new node(40);
-    head=insertatindex(head,7777,1);
+    head->next=new node(20);
+    head=insertatindex(head,77,7);
     printlist(head);
 }
