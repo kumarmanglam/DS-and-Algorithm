d1206cf4526c0b84173b8f7a648cf55ce36c2780
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-25 22:52:51 +0530

    updated

--- c++/LinkedList/06_insert_atBegin.cpp
+++ c++/LinkedList/06_insert_atBegin.cpp
@@ -8,24 +8,27 @@ struct node{
         data=x;
         next=NULL;
     }
+};
+
+node* insertatbegin(node *head,int x){
+    node *temp=new node(x);
+    temp->next=head;
+    return temp;
 }
 
-node* insertatbegin(node *first,int x){
-    node temp=new node(x);
-    temp->next=head;
-    return head;
+void printlist(node* head){
+    node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    return;
 }
 
 int main(){
     node* head=new node(4);
-    node*first=new node(8);
-    node* second=new node(29);
-    head->next=first;
-    first->next=second;
-    
-    //second way to create 
-    second->next=new node(89);
-    second->next->next=new node(34);
-    head=insert(head,800);
+    head=insertatbegin(head,8);
+    head=insertatbegin(head,80);
+    head=insertatbegin(head,800);
     printlist(head);
-}    
+} 
