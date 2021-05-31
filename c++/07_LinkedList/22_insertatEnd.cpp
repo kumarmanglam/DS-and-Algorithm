a40aec117be87027f049867d63ae41b4f0780038
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-05-31 22:10:07 +0530

    successfully inserted at the end of the linked list

---
+++ c++/07_LinkedList/22_insertatEnd.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+//my code
+node* insertatend(node* head,int x){
+    node* newly=new node(x);
+    if(head==NULL){
+        newly->next=newly;
+        return newly;
+    }
+    newly->data=head->data;
+    head->data=x;
+    newly->next=head->next;
+    head->next=newly;
+    return newly;
+}
+
+
+//naive solution 
+
+
+void printlist(node* head){
+    node* temp=head;
+    if(temp==NULL)
+        return;
+    do{                             //we use do while coz we have to stop at head
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }while(temp!=head);
+    return;
+}
+
+
+int main(){
+    node* head=new node(8);
+    head->next=new node(9);
+    head->next->next=new node(11);
+    head->next->next->next=head;
+    head=insertatend(head,33);
+    printlist(head);
+}
+
