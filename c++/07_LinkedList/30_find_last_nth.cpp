74a89633ee6654c01bd59d9747cf6feeefd54c8c
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-01 03:25:14 +0530

    some bug

--- c++/07_LinkedList/30_find_last_nth.cpp
+++ c++/07_LinkedList/30_find_last_nth.cpp
@@ -1,1 +1,56 @@
 
+#include <iostream>
+using namespace std;
+
+struct node {
+    int data;
+    node* next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void findnthnode(node* head,int n){
+    if(head==NULL) return ;
+    node* first=head,*second=head;
+    while(first!=NULL){
+        int i=0;
+        while(i<n){
+            first=first->next;
+            i++;
+        }
+        first=first->next;
+        second=second->next;
+        
+    }
+    cout<<second->data;
+}
+
+node* insertatbegin(node*head,int x){
+    node* temp=new node(x);
+    temp->next=head;
+    return temp;
+}
+
+
+void printlist(node* head){
+    node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    return;
+}
+
+int main(){
+    node*head=new node(4);
+    head->next=new node(8);
+    head=insertatbegin(head,56);
+    head=insertatbegin(head,45);
+    head=insertatbegin(head,115);
+    
+    printlist(head);
+    cout<<"\n";
+    findnthnode(head,2);
+}
