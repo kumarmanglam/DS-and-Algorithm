f37d6cdf0af52a0056b5fb385e67db05ffa3d9c6
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-02 02:56:05 +0530

    great success in detecting loop

---
+++ c++/07_LinkedList/39_fast_slow_detect_loop.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+bool checkloop(node*head){
+    node*fast=head,*slow=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    node* head=new node(4);
+    node* first=new node(5);
+    node*second=new node(6);
+    node* third=new node(7);
+    node*fourth=new node(8);
+    head->next=first;
+    first->next=second;
+    second->next=third;
+    third->next=fourth;
+    fourth->next=NULL;
+    cout<<checkloop(head);
+}
