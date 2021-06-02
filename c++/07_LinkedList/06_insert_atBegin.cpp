8921f99d50f4accddec454d5228ea1da244c39d8
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-03 00:33:08 +0530

    great sucess in odd even segregation

--- c++/07_LinkedList/06_insert_atBegin.cpp
+++ c++/07_LinkedList/06_insert_atBegin.cpp
@@ -10,10 +10,38 @@ struct node{
     }
 };
 
-node* insertatbegin(node *head,int x){
-    node *temp=new node(x);
-    temp->next=head;
-    return temp;
+node* oddeven(node* head){
+    node* odd=NULL,*even=NULL,*curr=head;
+    node*oddhead;
+    node*oddtail=NULL;
+    node*evenhead;
+    while(curr!=NULL){
+        if((curr->data)%2!=0){
+            if(odd==NULL){
+                odd=new node(curr->data);
+                oddhead=odd;
+            }
+            else{
+                odd->next=new node(curr->data);
+                oddtail=odd;
+                odd=odd->next;
+            }
+            curr=curr->next;
+        }
+        else{
+            if(even==NULL){
+                even=new node(curr->data);
+                evenhead=even;
+            }
+            else{
+                even->next=new node(curr->data);
+                even=even->next;
+            }
+            curr=curr->next;
+        }
+    }
+    odd->next=evenhead;
+    return oddhead;
 }
 
 void printlist(node* head){
@@ -26,9 +54,16 @@ void printlist(node* head){
 }
 
 int main(){
-    node* head=new node(4);
-    head=insertatbegin(head,8);
-    head=insertatbegin(head,80);
-    head=insertatbegin(head,800);
+    node* head=new node(1);
+    node* first=new node(3);
+    node*second=new node(5);
+    node* third=new node(2);
+    node*fourth=new node(11);
+    head->next=first;
+    first->next=second;
+    second->next=third;
+    third->next=fourth;
+    fourth->next=NULL;
+    head=oddeven(head);
     printlist(head);
-} 
+}
