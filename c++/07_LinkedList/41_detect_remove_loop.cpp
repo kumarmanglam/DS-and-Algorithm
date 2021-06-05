ef59b987333da79ec55c2e4eb3d8e94e3dad3aad
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-05 18:22:53 +0530

    Some problem in previous solution but the error still persists

--- c++/07_LinkedList/41_detect_remove_loop.cpp
+++ c++/07_LinkedList/41_detect_remove_loop.cpp
@@ -13,22 +13,48 @@ struct node{
     }
 };
 
-node* removeloop(node*head){
-    node*fast=head,*slow=head;
-    while(fast!=NULL && fast->next!=NULL){
+// void removeloop(node* head) 
+// { 
+//     node *slow = head, *fast = head; 
+  
+//     while (fast!=NULL && fast->next!=NULL) { 
+//         slow = slow->next; 
+//         fast = fast->next->next; 
+//         if (slow == fast) { 
+//             break; 
+//         } 
+//     } 
+//     if(slow!=fast)
+//         return;
+//     slow=head;
+//     while(slow->next!=fast->next){
+//         slow=slow->next;
+//         fast=fast->next;
+//     }
+//     fast->next=NULL;
+// }
+
+void detectRemoveLoop(Node* head) 
+{ 
+    Node *slow = head, *fast = head; 
+  
+    while (fast!=NULL && fast->next!=NULL) { 
+        slow = slow->next; 
+        fast = fast->next->next; 
+        if (slow == fast) { 
+            break; 
+        } 
+    } 
+    if(slow!=fast)
+        return;
+    slow=head;
+    while(slow->next!=fast->next){
         slow=slow->next;
-        fast=fast->next->next;
-        if(slow==fast){
-            slow=head;
-            while(slow->next!=fast->next){
-                slow=slow->next;
-                fast=fast->next;
-            }
-            fast->next=NULL;
-        }
+        fast=fast->next;
     }
-    return head;
+    fast->next=NULL;
 }
+
 
 void printlist(node* head){
     node*temp=head;
@@ -44,11 +70,37 @@ int main(){
     node*second=new node(6);
     node* third=new node(7);
     node*fourth=new node(8);
+    node*fifth=new node(9);
     head->next=first;
     first->next=second;
     second->next=third;
     third->next=fourth;
-    fourth->next=second;
-    head=removeloop(head);
+    fourth->next=fifth;
+    fifth->next=first;
+    removeloop(head);
+    printlist(head);
+}
+void printlist(node* head){
+    node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+int main(){
+    node* head=new node(4);
+    node* first=new node(5);
+    node*second=new node(6);
+    node* third=new node(7);
+    node*fourth=new node(8);
+    node*fifth=new node(9);
+    head->next=first;
+    first->next=second;
+    second->next=third;
+    third->next=fourth;
+    fourth->next=fifth;
+    fifth->next=first;
+    removeloop(head);
     printlist(head);
 }
