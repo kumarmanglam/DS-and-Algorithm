0d900a20df05cf3f575d727c84a94f55f6fec29e
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-08 20:04:47 +0530

    no bugs in efficient solution of stock span

--- c++/08_Stacks/1_Stock_span/efficient.cpp
+++ c++/08_Stacks/1_Stock_span/efficient.cpp
@@ -1,45 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
 using namespace std;
- 
-// Fills array S[] with span values
-void calculateSpan(int price[], int n, int S[])
-{
-    // Span value of first day is always 1
-    S[0] = 1;
- 
-    // Calculate span value of remaining days 
-    // by linearly checking previous days
-    for (int i = 1; i < n; i++)
-    {
-        S[i] = 1; // Initialize span value
- 
-        // Traverse left while the next element 
-        // on left is smaller than price[i]
-        for (int j = i - 1; (j >= 0) &&
-                (price[i] >= price[j]); j--)
-            S[i]++;
+
+//here in stack we store the index of the elementss
+//previous greater element 
+void stockspan(int arr[],int n){
+    stack <int> s;
+    s.push(0);
+    cout<<1<<" ";
+    int span=0;
+    for(int i=1;i<n;i++){
+        while(s.empty()==false && arr[s.top()]<=arr[i]){
+            s.pop();
+        }
+        span=s.empty()?i+1:i-s.top();
+        cout<<span<<" ";
+        s.push(i);
     }
 }
- 
-// A utility function to print elements of array
-void printArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-}
- 
-// Driver code
-int main()
-{
-    int price[] = { 10, 4, 5, 90, 120, 80 };
-    int n = sizeof(price) / sizeof(price[0]);
-    int S[n];
- 
-    // Fill the span values in array S[]
-    calculateSpan(price, n, S);
- 
-    // print the calculated span values
-    printArray(S, n);
- 
+
+int main(){
+    int arr[]={1,4,2,6,7,45,23,45,23,11,10,34};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    stockspan(arr,n);
     return 0;
 }
