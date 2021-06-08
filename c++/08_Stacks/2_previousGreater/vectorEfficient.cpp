435ad14fc28166a3b1d5aeb09e1bb228572ba058
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-08 21:23:25 +0530

    Bugs in vector output of previous greater

---
+++ c++/08_Stacks/2_previousGreater/vectorEfficient.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+
+//return type vector
+vector<int> previousgreater(int arr[],int n){
+    stack <int> s;
+    vector <int> v;
+    for(int i=0;i<n;i++){
+        int index=0;
+        while(s.empty()==false && arr[s.top()]<=arr[i]){
+            s.pop();
+        }
+        index=s.empty()? -1: s.top();
+        v.push_back(arr[index]);
+        s.push(i);
+    }
+    return v;
+}
+
+int main(){
+    int arr[]={1,45,23,11,10,34};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int>v;
+    v=previousgreater(arr,n);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i];
+    }
+    return 0;
+}
