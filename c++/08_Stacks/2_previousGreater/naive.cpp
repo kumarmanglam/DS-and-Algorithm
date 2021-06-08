c33b5b445806ad131201f778463d98a28887761c
kumarmanglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-08 21:00:36 +0530

    removed previous bug of -1 condition

--- c++/08_Stacks/2_previousGreater/naive.cpp
+++ c++/08_Stacks/2_previousGreater/naive.cpp
@@ -4,13 +4,17 @@ using namespace std;
 //naive solution to find previous greater element
 
 void previousgreater(int arr[],int n){
-    cout<<arr[0]<<" ";
+    cout<<-1<<" ";
     for(int i=1;i<n;i++){
-        for(int j=i-1;j>=0;j--){
-            if(arr[j]>=arr[i]){
+        int j;
+        for(j=i-1;j>=0;j--){
+            if(arr[j]>arr[i]){
                 cout<<arr[j]<<" ";
                 break;
             }
+        }
+        if(j==-1){
+            cout<<-1<<" ";
         }
     }
 }
