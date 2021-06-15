840ab444fb9a6cd489f8d6b4a18ff9dfc276ea44
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-15 19:39:57 +0530

    implemented by myself]

--- c++/10_Deque/1_intro/1_structure.cpp
+++ c++/10_Deque/1_intro/1_structure.cpp
@@ -1,12 +1,73 @@
 #include <iostream>
 using namespace std;
 
+//wap to implement a deque using a circular array 
+
 struct deque{
-    int size,cap;
+    int size,front,cap;
     int* arr;
-    deque(int c){
-        cap=c;
+    deque(int x){
+        cap=x;
         size=0;
-        arr=new int[cap];
-    }    
+        front=0;
+    }
+};
+
+bool isfull(){
+    return (size==cap);
+}
+
+bool isempty(){
+    return (size==0);
+}
+
+int getfront(){
+    if(isempty()){
+        cout<<" deque is empty \n";
+        return -1;
+    }
+    return arr[front];
+}
+
+int getback(){
+    if(isempty()){
+        cout<<" deque is empty \n";
+        return -1;
+    }
+    return arr[(front+size-1)/cap];
+}
+
+void pushfront(int x){
+    if(isfull()){
+        cout<<" deeque is full \n";
+        return ;
+    }
+    arr[front-1]=x;
+    return ;
+}
+
+void pushback(int x){
+    if(isfull()){
+        cout<< " deque is full \n";
+        return ;
+    }
+    arr[(front+size-1)/cap];
+    return;
+}
+
+void deletefront(){
+    if(isempty()){
+        cout<<" deque is empty";
+        return;
+    }
+    front=front+1;
+}
+
+void deleteback(){
+    if(isempty()){
+        cout<<" deque is empty";
+        return;
+    }
+    size=size-1;
+    return;
 }
