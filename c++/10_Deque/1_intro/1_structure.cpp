2847407dcb67a14b2e727ceb3307f1cc3aaff0af
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-15 21:32:10 +0530

    debugging in process

--- c++/10_Deque/1_intro/1_structure.cpp
+++ c++/10_Deque/1_intro/1_structure.cpp
@@ -12,66 +12,83 @@ struct deque{
         front=0;
         arr=new int[cap];
     }
+
+    
+    bool isfull(){
+        return (size==cap);
+    }
+    
+    bool isempty(){
+        return (size==0);
+    }
+    
+    int getfront(){
+        if(isempty()){
+            cout<<" deque is empty \n";
+            return -1;
+        }
+        return arr[front];
+    }
+    
+    int getback(){
+        if(isempty()){
+            cout<<" deque is empty \n";
+            return -1;
+        }
+        return arr[(front+size-1)/cap];
+    }
+    
+    void pushfront(int x){
+        if(isfull()){
+            cout<<" deeque is full \n";
+            return ;
+        }
+        arr[(front+cap-1)%cap]=x;                       // we take modulo here so that we do not get negative index imp front-cap-1 gives previous index to front
+        return ;
+    }
+    
+    void pushback(int x){
+        if(isfull()){
+            cout<< " deque is full \n";
+            return ;
+        }
+        arr[(front+size-1)/cap];
+        return;
+    }
+    
+    void deletefront(){
+        if(isempty()){
+            cout<<" deque is empty";
+            return;
+        }
+        front=front+1;
+    }
+    
+    void deleteback(){
+        if(isempty()){
+            cout<<" deque is empty";
+            return;
+        }
+        size=size-1;
+        return;
+    }
+    
+    void display(){
+        int i=front;
+        int j=front+size;
+        while(i<j){
+            cout<<arr[(i+cap)%cap]<<" ";
+            i++;
+        }
+    }
 };
 
-bool isfull(){
-    return (size==cap);
-}
-
-bool isempty(){
-    return (size==0);
-}
-
-int getfront(){
-    if(isempty()){
-        cout<<" deque is empty \n";
-        return -1;
-    }
-    return arr[front];
-}
-
-int getback(){
-    if(isempty()){
-        cout<<" deque is empty \n";
-        return -1;
-    }
-    return arr[(front+size-1)/cap];
-}
-
-void pushfront(int x){
-    if(isfull()){
-        cout<<" deeque is full \n";
-        return ;
-    }
-    front=(front+cap-1)%cap;
-    arr[front]=x;                       // we take modulo here so that we do not get negative index imp front-cap-1 gives previous index to front
-    size++;
-}
-
-void pushback(int x){
-    if(isfull()){
-        cout<< " deque is full \n";
-        return ;
-    }
-    arr[(front+size)/cap]=x;
-    size++;
-    return;
-}
-
-void deletefront(){
-    if(isempty()){
-        cout<<" deque is empty";
-        return;
-    }
-    front=(front+1)%cap;
-    size--;
-}
-
-void deleteback(){
-    if(isempty()){
-        cout<<" deque is empty";
-        return;
-    }
-    size=size-1;
-    return;
+int main(){
+    deque q(5);
+    q.pushback(34);
+    q.pushback(23);
+    q.pushfront(30);
+    q.pushfront(48);
+    q.display();
+    return 0;
 }
