7d058035070106633d5fd88742d82e3817d7170e
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-06-20 02:29:56 +0530

    Added iterative method of searching

--- c++/12_BST/1_intro/1_searching.cpp
+++ c++/12_BST/1_intro/1_searching.cpp
@@ -1,42 +1,54 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct node{
-    int key;
-    node* left;
-    node* right;
-    node(int k){
-        key=k;
-        left=right=NULL;
-    }
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
 };
-// searching in BST
-bool search(node* root,int k){
-    if(root==NULL){
+//recusive
+bool search(Node *root, int x){
+    if(root==NULL)
         return false;
-    }
-    if(root->key==k){
+    if(root->key==x)
         return true;
+    else if(root->key>x){
+        return search(root->left,x);
+    }else{
+        return search(root->right,x);
     }
-    if(root->key>k){
-        search(root->right,k);
+} 
+//iterative
+bool search(node* root,int x){
+    while(root!=NULL){
+        if(root->key==x)
+            return true;
+        else if((root->key)<x){
+            root=root->right;
+        }
+        else
+            root=root->left;
     }
-    if(root->key<k){
-        search(root->left,k);
-    }
+    return false;
 }
-//          35
-//         / \
-//        15 20
-//       /   /  \
-//      15  8   12
-
-int main(){
-    node* root=new node(9);
-    root->left=new node(12);
-    root->right=new node(14);
-    root->left->left=new node(13);
-    root->right->left=new node(23);
-    cout<<search(root,13);
-    return 0;
+int main() {
+	
+	Node *root=new Node(15);
+	root->left=new Node(5);
+	root->left->left=new Node(3);
+	root->right=new Node(20);
+	root->right->left=new Node(18);
+	root->right->left->left=new Node(16);
+	root->right->right=new Node(80);
+	int x=16;
+	
+	if(search(root,x))
+	    cout<<"Found";
+	else
+	    cout<<"Not Found";
 }
