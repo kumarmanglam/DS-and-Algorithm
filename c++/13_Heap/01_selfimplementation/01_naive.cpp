f4a325e3c943d45ab728a2742cae306edf6118c4
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-07-03 21:36:22 +0530

    self implementation of heap

--- c++/13_Heap/01_selfimplementation/01_naive.cpp
+++ c++/13_Heap/01_selfimplementation/01_naive.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class minheap{
+    int arr[];
+    int size;
+    int capacity;
+    minheap(int c){
+        arr[]=new int[c];
+        size=0;
+        capacity=c;
+    }
+    //int left(int i){return (2*i+1);
+    //int right(int i){return (2*i+2);
+    //int parent(int i){return (i-1)/2;
+}
 
