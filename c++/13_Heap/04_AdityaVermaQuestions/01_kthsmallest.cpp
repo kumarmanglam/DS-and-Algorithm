375327da9515fc625b7fb19f04556e785030bcaf
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-07-04 03:08:24 +0530

    Ksmallest pop when size is greater than k

---
+++ c++/13_Heap/04_AdityaVermaQuestions/01_kthsmallest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int kthsmallest(int arr[],int n,int k){
+    priority_queue <int> pq;
+    for(int i=0;i<n;i++){
+        pq.push(arr[i]);
+        if(pq.size()>3){
+            pq.pop();
+        }
+    }
+    return pq.top();
+}
+
+int main(){
+    int arr[]={34,76,15,35,10,20,50};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=3;
+    cout<<kthsmallest(arr,n,k);
+    return 0;
+}
