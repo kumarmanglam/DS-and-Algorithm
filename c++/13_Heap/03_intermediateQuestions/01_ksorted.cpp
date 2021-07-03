d4c4be00279868c095c0dc841bab876159fa36c6
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-07-04 03:49:11 +0530

    Update 01_ksorted.cpp

--- c++/13_Heap/03_intermediateQuestions/01_ksorted.cpp
+++ c++/13_Heap/03_intermediateQuestions/01_ksorted.cpp
@@ -5,8 +5,8 @@ int sortK(int arr[], int n, int k)
 { 
 	priority_queue<int, vector<int>, greater<int> > pq; 
     
-    for(int i=0;i<=k;i++)
-        pq.push(arr[i]);
+	for(int i=0;i<=k;i++)
+		pq.push(arr[i]);
         
 	int index = 0; 
 	for (int i = k + 1; i < n; i++) { 
