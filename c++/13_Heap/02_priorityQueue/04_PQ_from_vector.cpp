35b82c8548bf2772161ed3143601fb48d61d7eaa
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-07-03 22:23:14 +0530

    creating priority queue from vector of int elements

---
+++ c++/13_Heap/02_priorityQueue/04_PQ_from_vector.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+//how to create a priority queue form a given a array of elements
+
+int main(){
+    while(pq.empty()==false){
+        vector <int> v={14.23,2};
+        priority_queue<int>pq(vec.begin(),vec.end()):
+        
+        while(!pq.empty()){
+            cout<<pq.top()<<" ";
+            pq.pop();
+        }
+    }
+    return 0;
+}
