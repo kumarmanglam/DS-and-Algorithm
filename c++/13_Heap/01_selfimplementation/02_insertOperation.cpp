c8f84de677417f39e136d9c9a6265328e90bce38
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-07-03 21:48:00 +0530

    insert a node in min heap using array

--- c++/13_Heap/01_selfimplementation/02_insertOperation.cpp
+++ c++/13_Heap/01_selfimplementation/02_insertOperation.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+using namespace std;
+
+class minheap{
+    int arr[];
+    int size;
+    int capacity;
+    minheap(int c){
+        arr[]=new int[c];
+        size=0;
+        capacity=c;
+    }
+    int left(int i){return (2*i+1);
+    int right(int i){return (2*i+2);
+    int parent(int i){return (i-1)/2;
+    
+    
+    insert(int x){
+        if(size==capacity)return ;
+        size++;
+        arr[size-1]=x;
+        for(int i=size-1;i!=0&&arr[parent[i])>arr[i]){
+            swap(arr[i],arr[parent(i)]);
+            i=parent(i);
+        }
+    }
+}
+
