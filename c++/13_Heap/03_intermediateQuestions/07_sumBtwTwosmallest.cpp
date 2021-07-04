30d1abe644af6b6d0d69187ee1bc03de4b47790d
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-07-05 00:46:12 +0530

    sum of numbers between k1 smallest and k2 smallest * redundant method

--- c++/13_Heap/03_intermediateQuestions/07_sumBtwTwosmallest.cpp
+++ c++/13_Heap/03_intermediateQuestions/07_sumBtwTwosmallest.cpp
@@ -1,1 +1,31 @@
 
+#include <iostream>
+#include <queue>
+using namespace std;
+
+//sum of elements between k1 smallest k2 smallest 
+//sum of numbers between k1 smallest and k2 smallest * redundant method
+void sums(int arr[],int n,int k,int m){
+    priority_queue<int,vector<int>,greater<int> > heap;
+    for(int i=0;i<n;i++){
+        heap.push(arr[i]);
+    }
+    int j=0;
+    for(;j<k;j++){
+        heap.pop();
+    }
+    int sum=0;
+    for(;j<m-1;j++){
+        sum+=heap.top();
+        heap.pop();
+    }
+    cout<<sum;
+}
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    
+    sums(arr,n,3,6);
+    return 0;
+}
