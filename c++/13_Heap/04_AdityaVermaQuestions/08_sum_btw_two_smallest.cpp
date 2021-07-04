ed9cdbaa086f1660d035955296b3bca71d126b5f
Kumar Manglam <54842342+kumarmanglam@users.noreply.github.com>
2021-07-05 00:56:39 +0530

    sum btw k1 smallest and k2 smallest * use Kthsmallest function

--- c++/13_Heap/04_AdityaVermaQuestions/08_sum_btw_two_smallest.cpp
+++ c++/13_Heap/04_AdityaVermaQuestions/08_sum_btw_two_smallest.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include <queue>
+using namespace std;
+
+//sum of elements between k1 smallest k2 smallest 
+//naive approach
+int Kthsmallest(int arr[],int n,int k){
+    priority_queue<int > heap;
+    
+    for(int i=0;i<n;i++){
+        heap.push(arr[i]);
+        if(heap.size()>k)
+            heap.pop();
+    }
+    return heap.top();
+}
+
+int sumbtw(int arr[],int n,int k,int m){
+    int sum1=Kthsmallest(arr,n,k);
+    int sum2=Kthsmallest(arr,n,m);
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>sum1 && arr[i]<sum2){
+            sum+=arr[i];
+        }
+    }
+    return sum;
+}
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    
+    cout<<sumbtw(arr,n,3,6);
+    return 0;
+}
